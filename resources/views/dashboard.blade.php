73bc01773ed6c9bbcd5fbf1e57249de4126ba0c7
Guyaume Tremblay <guyaume.tremblay@ellipse-synergie.com>
2017-05-19 15:02:27 -0400

    Add analytics of Ellipse, Psylio Canada and Epsylio websites

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,9 +5,12 @@
     @javascript(compact('pusherKey', 'pusherCluster', 'usingNodeServer'))
 
     <div class="dashboard" id="dashboard">
+        <analytics website="Ellipse" grid="a1:b1" view="51745830"></analytics>
+        <analytics website="Psylio Canada" grid="a2:b2" view="115161520"></analytics>
+        <analytics website="Epsylio" grid="a3:b3" view="107066110"></analytics>
+
         <current-time grid="e1" dateformat="ddd DD/MM"></current-time>
         <packagist-statistics grid="e2"></packagist-statistics>
-
         <internet-connection></internet-connection>
     </div>
 @endsection
